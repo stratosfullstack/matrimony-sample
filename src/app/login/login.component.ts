68db125494c12a2be2a78185c325207207cd3a36
stratosfullstack <stratosfullstack@gmail.com>
2024-11-12 13:39:21 +0530

    loginmethod1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,62 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email_id: string = "";
+  psw: string = "";
+
+  userDetails: any[] = [
+    { user_id: 1000, email_id: "libin@gmail.com", password: "123" },
+    { user_id: 1001, email_id: "manu@gmail.com", password: "123" },
+    { user_id: 1002, email_id: "riya@gmail.com", password: "123" },
+    { user_id: 1003, email_id: "ninu@gmail.com", password: "123" },
+    { user_id: 1004, email_id: "jeena@gmail.com", password: "123" }
+  ];
 
   constructor() { }
 
-  login() {
-    alert("login clicked")
+  //   login() {
+  //     const user = this.userDetails.find(user => user.email_id === this.email_id);
+  //     if (user) {
+  //       if (user.password === this.psw) {
+  //         alert("Login successful");
+  //       } else {
+  //         alert("Incorrect password");
+  //       }
+  //     } else {
+  //       alert("Incorrect email ID");
+  //     }
+  //   }
+
+  //   emailChange(event: any) {
+  //     this.email_id = event.target.value;
+  //   }
+
+  //   pswChange(event: any) {
+  //     this.psw = event.target.value;
+  //   }\
+
+
+
+  login(a: any, b: any) { 
+    this.email_id = a.value
+    this.psw = b.value
+    const user = this.userDetails.find(user => user.email_id === this.email_id);
+    if (user) {
+      if (user.password === this.psw) {
+        alert("Login successful");
+      } else {
+        alert("Incorrect password");
+      }
+    } else {
+      alert("Incorrect email ID");
+    }
+  }
+
+  emailChange(event: any) {
+    this.email_id = event.target.value;
+  }
+
+  pswChange(event: any) {
+    this.psw = event.target.value;
   }
 }
