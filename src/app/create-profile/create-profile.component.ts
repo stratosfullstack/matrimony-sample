722bac9157b8908ce15a87c182236e357238827a
stratosfullstack <stratosfullstack@gmail.com>
2024-12-20 15:47:12 +0530

    additional fields in create profile

--- src/app/create-profile/create-profile.component.ts
+++ src/app/create-profile/create-profile.component.ts
@@ -218,6 +218,68 @@ export class CreateProfileComponent {
     "Supply Chain Manager",
     "Welder"];
 
+  educations = [
+    "Primary Education",
+    "Middle School",
+    "High School (SSC/ Matriculation)",
+    "Higher Secondary (HSC/ Intermediate)",
+    "Bachelor of Arts (BA)",
+    "Bachelor of Science (BSc)",
+    "Bachelor of Commerce (BCom)",
+    "Bachelor of Engineering (BE)",
+    "Bachelor of Technology (BTech)",
+    "Bachelor of Business Administration (BBA)",
+    "Bachelor of Computer Applications (BCA)",
+    "Bachelor of Laws (LLB)",
+    "Bachelor of Architecture (BArch)",
+    "Bachelor of Design (BDes)",
+    "Bachelor of Fine Arts (BFA)",
+    "Bachelor of Pharmacy (BPharm)",
+    "Master of Arts (MA)",
+    "Master of Science (MSc)",
+    "Master of Commerce (MCom)",
+    "Master of Engineering (ME)",
+    "Master of Technology (MTech)",
+    "Master of Business Administration (MBA)",
+    "Master of Computer Applications (MCA)",
+    "Master of Laws (LLM)",
+    "Master of Architecture (MArch)",
+    "Master of Design (MDes)",
+    "Master of Fine Arts (MFA)",
+    "Master of Pharmacy (MPharm)",
+    "Doctor of Philosophy (PhD)",
+    "Doctor of Literature (DLitt)",
+    "Doctor of Science (DSc)",
+    "Doctor of Laws (LLD)",
+    "Diploma in Engineering",
+    "Diploma in Technology",
+    "Diploma in Management",
+    "Diploma in Computer Applications",
+    "Certificate in Vocational Courses",
+    "Certificate in Language Courses",
+    "Chartered Accountancy (CA)",
+    "Company Secretaryship (CS)",
+    "Cost and Management Accountancy (CMA)",
+    "Indian Institute of Management (IIM)",
+    "Indian Institute of Technology (IIT)",
+    "National Institute of Open Schooling (NIOS)",
+    "International Baccalaureate (IB)",
+    "Cambridge International Examinations (CIE)",
+    "Other international qualifications"
+  ]
+
+  countryCodes = [
+    996, 995, 994, 992, 98, 977, 975, 974, 973, 972, 970, 968, 966, 965, 964, 962, 961, 960,
+    95, 93, 92, 91, 880, 86, 856, 855, 853, 852, 850, 82, 81, 7, 692, 690, 689, 687, 686,
+    685, 683, 682, 680, 679, 676, 675, 674, 673, 670, 64, 63, 62, 61, 60, 599, 596, 595, 594,
+    593, 592, 591, 590, 57, 56, 55, 54, 53, 51, 509, 508, 507, 506, 505, 504, 503, 502, 501,
+    500, 49, 48, 47, 45, 43, 423, 420, 39, 389, 387, 385, 378, 376, 375, 374, 372, 371, 370,
+    36, 359, 358, 357, 356, 355, 354, 353, 352, 351, 350, 33, 32, 31, 30, 299, 298, 297, 291,
+    269, 267, 266, 265, 264, 262, 261, 258, 257, 255, 254, 253, 251, 250, 245, 244, 241, 240,
+    239, 238, 237, 236, 235, 234, 233, 231, 230, 229, 228, 227, 226, 225, 224, 223, 222, 220,
+    218, 213, 212, 20, 1
+  ];
+
   createProfileForm: FormGroup;
 
   constructor(private router: Router, private ds: DataService, private fb: FormBuilder) {
@@ -227,7 +289,12 @@ export class CreateProfileComponent {
       gender: ['', [Validators.required]],
       height: ['', [Validators.required]],
       country: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      district: ['', [Validators.required]],
+      place: ['', [Validators.required]],
+      countryCode: ['', [Validators.required]],
       phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      education: ['', [Validators.required]],
       occupation: ['', [Validators.required, Validators.minLength(5)]],
       diocese: ['', [Validators.required]],
       parish: ['', [Validators.required]],
@@ -240,7 +307,7 @@ export class CreateProfileComponent {
   }
 
   selectedImage: any;
-  selectedPdf:any;
+  selectedPdf: any;
 
   storedData: string | null = typeof localStorage !== 'undefined'
     ? JSON.parse(localStorage.getItem('currentEmailid') ?? 'null')
@@ -264,8 +331,12 @@ export class CreateProfileComponent {
       formData.append('gender', this.createProfileForm.value.gender);
       formData.append('height', this.createProfileForm.value.height);
       formData.append('country', this.createProfileForm.value.country);
-      formData.append('phone', this.createProfileForm.value.phone);
+      formData.append('state', this.createProfileForm.value.state);
+      formData.append('district', this.createProfileForm.value.district);
+      formData.append('place', this.createProfileForm.value.place);
+      formData.append('countryCode', this.createProfileForm.value.countryCode);
       formData.append('email', this.email ?? '');
+      formData.append('education', this.createProfileForm.value.education);
       formData.append('occupation', this.createProfileForm.value.occupation);
       formData.append('diocese', this.createProfileForm.value.diocese);
       formData.append('parish', this.createProfileForm.value.parish);
